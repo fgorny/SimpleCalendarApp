c809dbd0341ea93f1b6d37b7ce1b07979b55696c
Filip Górny <fil.gorny@gmail.com>
2020-02-07 12:50:07 +0100

    Initial commit

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btn1_click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Calendar cal = new Calendar();
+            cal.DisplayDateStart = new DateTime(2020, 02, 07);
+            cal.DisplayDateEnd = new DateTime(2030, 12, 31);
+            cal.DisplayDate = new DateTime(2015, 01, 01);
+        }
+    }
+}
